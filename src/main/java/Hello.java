2ac3acb8acc9c5ab3c58701ba922d931f4f38acd
lcf767482805 <lcf767482805@163.com>
2019-06-27 15:28:45 +0800

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+*@Author:lcf
+*@Date:2019/6/27 15:27
+*/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
